01bde5ce18b29dab72de4329559891edc4162362
Siutan <mukil033@gmail.com>
2025-08-02 16:09:01 +1000

    Initial Commit

---
+++ coverme/covermeApp.swift
@@ -0,0 +1,17 @@
+//
+//  covermeApp.swift
+//  coverme
+//
+//  Created by Mukil Chittybabu on 2/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct covermeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ covermeTests/covermeTests.swift
@@ -0,0 +1,17 @@
+//
+//  covermeTests.swift
+//  covermeTests
+//
+//  Created by Mukil Chittybabu on 2/8/2025.
+//
+
+import Testing
+@testable import coverme
+
+struct covermeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
